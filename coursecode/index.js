2a27518f6971d2db3947627c7a2807804e852a99
Gonçalo Costa 45371 <mailgc99@gmail.com>
2022-10-05 15:25:33 +0100

    basic notions done

--- coursecode/index.js
+++ coursecode/index.js
@@ -1,1 +1,50 @@
 console.log("Hello world!");
+
+//cannot be reserved keywords
+//must be significant
+//does not start with a number
+//cammelNotation
+//case sensitive
+let name;
+
+console.log(name); //undefined non-initialized
+
+let newName = "Mosh";
+
+console.log(newName); //Mosh
+
+//-----------------
+
+let interestRate = 0.3; //can be changed
+const interestRateUpdated = 1; //cannot be changed
+
+let age = 30;
+console.log(typeof age);
+age = "thirty";
+console.log(typeof age);
+
+//-----------------
+
+let person = {
+  name: "Steve",
+  age: 20,
+};
+
+console.log(person);
+
+//-----------------
+
+let selectedColors = ["red", "blue"];
+selectedColors[2] = 1;
+
+console.log(selectedColors);
+
+console.log(selectedColors.length);
+
+//-----------------
+
+function greet() {
+  console.log("hello world!!");
+}
+
+greet();
